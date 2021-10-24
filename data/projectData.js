89178df148c78f6be8768ac9d4fa3108d4430173
Grandpaonfire <79377394+Grandpaonfire@users.noreply.github.com>
2021-10-24 23:07:53 +0100

    Add files via upload

---
+++ data/projectData.js
@@ -0,0 +1,58 @@
+const projectData = [
+  {
+    id: 1,
+    image: '/projectImages/relic.png',
+    title: "Relic",
+    build: "React, Next.js, AWS Amplify, SASS & CSS Modules",
+    url: "https://beta.relicapp.co.uk",
+    target: "https://beta.relicapp.co.uk",
+    description: "During this internship I replicated a designer's website wireframe, creating the UI with React for a new Tech start-up. I utilised Git and setup the front-end to connect with the AWS Amplify back-end and dynamoDB"
+  },
+  {
+    id: 2,
+    title: "Posh Tarts",
+    image: '/projectImages/posh-tarts.png',
+    build: "React, Next.js, TailwindCSS & SendGrid Email API",
+    url: "https://posh-tarts.co.uk",
+    target: "https://posh-tarts.co.uk",
+    description: "I created this website for a small local catering business. The website allows clients to contact the business owners directly through a contact form, whilst giving users a look at the company's menus, food and reviews"
+  },
+  {
+    id: 3,
+    title: "Quiz App",
+    image: '/projectImages/random-quiz.png',
+    build: "React, Typescript, Styled-Components & OpenTrivia API",
+    url: "https://quickrandomquiz.netlify.app",
+    target: "https://quickrandomquiz.netlify.app",
+    description: "This small Typescript project uses the OpenTrivia API with the React useState hook to create a short 10 question quiz"
+  },
+  {
+    id: 4,
+    title: "Web Weaver",
+    image: '/projectImages/web-weaver.png',
+    build: "React, Next.js, Material-Ui, Reactstrap & SendGrid Email API",
+    url: "https://www.thewebweaver.co.uk",
+    target: "https://www.thewebweaver.co.uk",
+    description: "This single page static site was created to attract clients in need of website's for their small businesses. The spider landing animation was made entirely with CSS transitions"
+  },
+  {
+    id: 5,
+    title: "Quick Docs",
+    image: '/projectImages/quick-docs.png',
+    build: "React, Next.js, Next-Auth, Google Firebase, React-Icons, Draft-js & TailwindCSS",
+    url: "https://quick-docs.vercel.app",
+    target: "https://quick-docs.vercel.app",
+    description: "This Google Docs clone uses Next-Auth for User verification with Google, and uses Draft-js and Google's Firestore to allow users to create documents with auto-save on exit"
+  },
+  {
+    id: 6,
+    title: "Dictionary",
+    image: '/projectImages/dictionary.png',
+    build: "React, Next.js, Material-Ui, CSS Modules & Axios",
+    url: "https://wee-dictionary.netlify.app",
+    target: "https://wee-dictionary.netlify.app",
+    description: "A simple dictionary application that uses Axios with the Dictionary API to allow the user to search definitions with synonyms and examples in multiple languages"
+  },
+];
+
+export default projectData;
